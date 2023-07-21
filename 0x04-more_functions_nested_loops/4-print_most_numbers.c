3b1cad5a7f869588cff554e8243d1d0e7119ec0c
youssefelsayed47 <youssefjo4433@gmail.com>
2023-07-20 19:28:31 -0700

    NICE

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,11 @@
 # include "main.h"
 
-/*
+/**
  * print_most_numbers - print 0 - 9 apart
  *		from 2 and 4 and you can
  *		only use _putcahr twice
  * Return: Always 0 (Success)
-*/
-
+ */
 void print_most_numbers(void)
 {
 	int num;
