51433f87618686e87b49ba3d303f583600f1d2b9
youssefelsayed47 <youssefjo4433@gmail.com>
2023-07-20 19:24:11 -0700

    NICE

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,10 @@
  * mul - multiples two integers
  *
  * @a: first input
- * @b second input
+ * @b: second input
  *
  * Return: return results
-*/
-
+ */
 int mul(int a, int b)
 {
 	return (a * b);
