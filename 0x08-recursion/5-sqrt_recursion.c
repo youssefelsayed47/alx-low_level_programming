4cc498f52d1b017e6be8902ec7d2beeff5b48de2
youssefelsayed47 <youssefjo4433@gmail.com>
2023-08-31 07:36:17 -0700

    DONE

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 
-/***/
+/**
+ * sqrt_2 - helper function
+ *
+ * @num: number to calc the square
+ * @count: number to count over to find square
+ * Return: the root of the number
+*/
+
+int sqrt_2(int num, int count)
+{
+	int square = count * count;
+
+	if (square == num)
+		return (count);
+	else if (square < num)
+		sqrt_2(num, count + 1);
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - a function that returns the natural
+ *		square root of a number.
+ * @n: the number to calc the square
+ * Return: the square of the number
+*/
 
 int _sqrt_recursion(int n)
 {
-
+	return (sqrt_2(n, 0));
 }
