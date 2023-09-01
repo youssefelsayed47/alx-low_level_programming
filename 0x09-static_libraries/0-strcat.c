218f29d30df14151d70af879eded4c506789e2f1
youssefelsayed47 <youssefjo4433@gmail.com>
2023-09-01 14:22:59 -0700

    DONE

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenate
+ *		two stringe.
+ *
+ * @dest: pointer to destenation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+}
