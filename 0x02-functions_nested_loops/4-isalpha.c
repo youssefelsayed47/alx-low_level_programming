c7a5cb60423ffa9718f812e1a66149e6fe0c7715
youssefelsayed47 <youssefjo4433@gmail.com>
2023-09-07 13:18:33 -0700

    DONE

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,9 @@
 
 int _isalpha(int c)
 {
-	if (c <= 'z' && c >= 'a' || c <= 'Z' && c >= 'A')
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	else if (c <= 'Z' && c >= 'A')
 		return (1);
 	return (0);
 }
