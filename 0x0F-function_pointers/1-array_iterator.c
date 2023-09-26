2b36ebd21f5cb970104cfb4a29b7bcfc124d4c7e
youssefelsayed47 <youssefjo4433@gmail.com>
2023-09-26 13:59:07 -0700

    DONE

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i = 0;
+	unsigned long int i = 0;
 
-	while (i < size)
-	{
-		action(*array);
-		array++;
-		i++;
-	}
+	if (array && action && size)
+		while (i < size)
+		{
+			action(*array);
+			array++;
+			i++;
+		}
 }
